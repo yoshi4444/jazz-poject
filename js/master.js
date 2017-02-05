c4250d50d54c99e6b5a9fefb9caee943318f2d3b
Joshua Davis <jdavis@xologic.com>
2017-02-05 09:46:47 -0700

    no message

--- js/master.js
+++ js/master.js
@@ -58,9 +58,9 @@ $(document).ready(function()
         var sectionTwo = $('.section2').offset().top;
         var sectionThree = $('.section3').offset().top;
         var sectionFour = $('.section4').offset().top;
-        var windowLocal = $(window).scrollTop();
+        var windowLocal = topNav;
         
-        if (sectionOne == windowLocal)
+        if (sectionOne > windowLocal)
         {
             setTimeout(function()
             {
@@ -70,7 +70,7 @@ $(document).ready(function()
                 $('.rightSectionText').removeClass('fadeInRightBig, rotateInDownRight, fadeInUpBig').hide();
             },500);
         }
-        else if (sectionTwo == windowLocal)
+        else if (sectionTwo > windowLocal)
         {
             setTimeout(function()
             {
@@ -82,7 +82,7 @@ $(document).ready(function()
                 $('.rightSectionText').removeClass('fadeOutRightBig');
             },500);
         }
-        else if (sectionThree == windowLocal)
+        else if (sectionThree > windowLocal)
         {
             setTimeout(function()
             {
@@ -92,7 +92,7 @@ $(document).ready(function()
                 $('.section3 .rightSectionText').addClass('rotateInDownRight animated').show();
             }, 500);
         }
-        else if (sectionFour == windowLocal)
+        else if (sectionFour > windowLocal)
         {
             setTimeout(function()
             {
