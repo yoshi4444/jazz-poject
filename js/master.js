1b3d346572200ec39856bce783a7767f4ca8f33b
Joshua Davis <jdavis@xologic.com>
2017-02-04 20:57:51 -0700

    Beginning of Something New

---
+++ js/master.js
@@ -0,0 +1,66 @@
+$(document).ready(function()
+{
+    /* Start Function for scroll to top Btn */
+    
+    var navTop = $('#bodySection').offset().top;
+    var scrollNav = function() {
+        var scrollToTop = $(window).scrollTop();
+        
+        if(scrollToTop > navTop) 
+        {
+            $('#scrollTopBtn').addClass('show');
+        }
+        else
+        {
+            $('#scrollTopBtn').removeClass('show');
+        }
+    };
+    scrollNav();
+    
+    $(window).scroll(function(){ scrollNav(); });
+    
+    $('#scrollTopBtn').click(function()
+    {
+        $('html, body').animate({ scrollTop:0 }, 600);
+    });
+    
+    /* End Function for scroll to top Btn */
+    
+    /* Start Hide or Show Header on Scroll */
+    
+    var topNav = 0;
+    
+    $(window).scroll(function () {
+        var windowPage = $(this).scrollTop();
+        if (windowPage > topNav) {
+            $('#topNavFull').slideUp();
+        } else {
+           $('#topNavFull').slideDown();
+        }
+        topNav = windowPage;
+    });
+    
+    /* End Hide or Show Header on Scroll */
+    
+    /* Start add activeBtn for sideBtn */
+    $('.sideBtn').click(function()
+    {
+        $(this).addClass('activeBtn');
+    });
+    
+    
+    
+    /* End add activeBtn for sideBtn */
+    
+});
+
+function changeSection(e)
+{
+    var sectionName = $(e).attr('data-name');
+    $('html, body').animate({
+    scrollTop: $('.'+sectionName).offset().top - 50
+    }, 500);
+    $('.sideBtn').removeClass('activeBtn');
+}
+
+
